bb56ae92f7fc619f0cab1140117fccfc2b2f9de8
jonggun.gim <jonggun.gim@gmail.com>
2021-09-03 06:18:57 +0900

    router link + view

---
+++ vue-book/src/views/DataBinding.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <h1>Hello {{title}}</h1>
+
+
+    <div>{{htmlString}}</div>
+
+    <div v-html='htmlString'></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'World',
+            htmlString: "<p style='color:red;'> This is a red string</p>"
+        };
+    }
+}
+</script>
