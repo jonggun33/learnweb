0b84285884db6578b9bd924649fff78ca1f30264
jonggun33 <jonggun.gim@gmail.com>
2023-10-20 14:17:02 +0900

    const {article} = this.props;

--- react_training/news/src/NewsItem.js
+++ react_training/news/src/NewsItem.js
@@ -2,25 +2,21 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    const article = {
-      "author": null,
-      "type": "IT",
-      "typeCode": "IT",
-      "description": "aaaa",
-      "url": "https://n.news.naver.com/article/023/0003794431?cds=news_media_pc",
-      "title": "asdafsd asdfasdfadf",
-      "urlToImage": "https://imgnews.pstatic.net/image/023/2023/10/20/0003794431_001_20231020101201046.jpg?type=w647"
-    }
+    const {article} = this.props;
     return (
       <>
         <li className="list_group-item">
-          <div>
-            <img src = {article.urlToImage} />
+          <div className="media">
+            {article.urlToImage && (
+              <img src={article.urlToImage} className="w-25 p-2" />
+            )}
           </div>
-      <h5>
-      <a href = {article.url}> {article.title} </a>
-      </h5>
-      {article.description}
+          <div className="media-body p-2">
+            <h5 className="mt-0">
+              <a href={article.url}> {article.title} </a>
+            </h5>
+            {article.description}
+          </div>
         </li>
       </>
     );
